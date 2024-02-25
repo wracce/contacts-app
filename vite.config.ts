dffc1377e9d5f5403d9e15fd11c90f795c88861a
Vitaliy <wracce@gmail.com>
2024-02-25 14:11:09 +0400

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
 import react from "@vitejs/plugin-react-swc"
 import path from "path"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@public": path.resolve(__dirname, "./public"),
-      "@app": path.resolve(__dirname, "./src/app"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@widgets": path.resolve(__dirname, "./src/widgets"),
-      "@features": path.resolve(__dirname, "./src/features"),
-      "@entities": path.resolve(__dirname, "./src/entities"),
-      "@shared": path.resolve(__dirname, "./src/shared"),
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+
+  return {
+    base: process.env.VITE_BASE,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@public": path.resolve(__dirname, "./public"),
+        "@app": path.resolve(__dirname, "./src/app"),
+        "@pages": path.resolve(__dirname, "./src/pages"),
+        "@widgets": path.resolve(__dirname, "./src/widgets"),
+        "@features": path.resolve(__dirname, "./src/features"),
+        "@entities": path.resolve(__dirname, "./src/entities"),
+        "@shared": path.resolve(__dirname, "./src/shared"),
+      },
     },
-  },
+  }
 })
